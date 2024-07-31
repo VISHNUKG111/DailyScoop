a5a4e20aace515a19bc77f65bb94fa1c86819cae
Ren-Jacob-John <141657080+Ren-Jacob-John@users.noreply.github.com>
2024-07-31 11:53:44 +0530

    Update Article.kt

--- app/src/main/java/com/example/dailyscoop/model/Article.kt
+++ app/src/main/java/com/example/dailyscoop/model/Article.kt
@@ -1,6 +1,18 @@
 package com.example.dailyscoop.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(
+
+    tableName = "articles"
+
+)
+
 data class Article(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int? =null,
     val author: String,
     val content: String,
     val description: String,

6a2b14328b85c857fcd70370fad51a4d89ceafd0
Ren-Jacob-John <141657080+Ren-Jacob-John@users.noreply.github.com>
2024-07-31 11:06:24 +0530

    add

---
+++ app/src/main/java/com/example/dailyscoop/api/NewsAPI.kt
@@ -0,0 +1,31 @@
+package com.example.dailyscoop.api
+
+import com.example.dailyscoop.model.NewsResponse
+import com.example.dailyscoop.util.Constants.Companion.API_KEY
+import okhttp3.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+
+interface NewsAPI {
+    @GET("v2/top-headlines")
+    suspend fun getHeadlines(
+        @Query("country")
+        countryCode: String = "us",
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apikey")
+        apiKey: String = API_KEY
+    ): Response<NewsResponse>
+
+    @GET("v2/everything")
+    suspend fun searchForNews(
+        @Query("q")
+        searchQuery: String,
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apikey")
+        apiKey: String = API_KEY
+    ): Response<NewsResponse>
+
+}

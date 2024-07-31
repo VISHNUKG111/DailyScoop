030009399e2542d561a0acc031463631fc3b5246
Ren-Jacob-John <141657080+Ren-Jacob-John@users.noreply.github.com>
2024-07-31 10:35:36 +0530

    add

---
+++ app/src/main/java/com/example/dailyscoop/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.dailyscoop.util
+
+class Constants {
+    companion object{
+        const val API_KEY ="947f3ff6cc914d5eb5636969109f112c"
+        const val BASE_URL ="https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY ="500"
+        const val QUERY_PAGE_SIZE ="20"
+    }
+
+}

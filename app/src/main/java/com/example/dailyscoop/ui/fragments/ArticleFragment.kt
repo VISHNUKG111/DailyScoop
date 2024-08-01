6d90056d7c28669541c6a0474a8aecf39bf74760
savio139 <savioshanto91@gmail.com>
2024-08-01 16:40:33 +0530

    add

--- app/src/main/java/com/example/dailyscoop/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/example/dailyscoop/ui/fragments/ArticleFragment.kt
@@ -1,4 +1,32 @@
 package com.example.dailyscoop.ui.fragments
 
-class ArticleFragment {
+import javax.naming.Binding
+import
+
+class ArticleFragment : Fragment(R.layout.fragment_article) {
+
+    lateinit var newsViewmodel: NewsViewsModel
+    val args: ArticleFragmentArgs by navArgs()
+    lateinit var binding: FragmentArticleBinding
+
+    override fun onViewCreated(view, savedInstanceState: bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentArticleBinding.bind(view)
+
+        NewsViewsModel = (activity as NewsActivity)newsViewModel
+        val article = args.article
+
+        binding.webview.apply {
+            webViewClient = WebVewClient()
+            article.url.let {
+                loadurl(it)
+            }
+        }
+
+        binding.fab.setOnClickListener{
+            newsViewmodel.addToFavourites(article)
+            Snackbar.make(view, "added to favourites", Snackbar.make(view, text:"added to favourites", Snackbar.LENGTH_SHORT)Show()
+        }
+    }
+
 }

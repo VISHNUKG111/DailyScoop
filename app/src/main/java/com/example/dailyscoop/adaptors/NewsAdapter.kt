83e46b3e907b13313f59d55c2ecaf0f535a4dc1f
rohithpb <pbrohith2@gmail.com>
2024-08-01 16:42:48 +0530

    add

--- app/src/main/java/com/example/dailyscoop/adaptors/NewsAdapter.kt
+++ app/src/main/java/com/example/dailyscoop/adaptors/NewsAdapter.kt
@@ -14,22 +14,22 @@ import com.example.dailyscoop.model.Article
 class NewsAdapter: RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
 
     inner class ArticleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
     lateinit var articleImage: ImageView
     lateinit var articleSource: TextView
     lateinit var articleTitle: TextView
     lateinit var articleDescription: TextView
     lateinit var articleDateTime: TextView
 
+    private val differCallback = object : DiffUtil.ItemCallback<Article>() {
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem.url == newItem.url
+        }
 
-      private val differCallback = object : DiffUtil.ItemCallback<Article>(){
-          override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
-              return oldItem.url == newItem.url
-          }
-
-          override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
-             return oldItem == newItem
-          }
-      }
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem == newItem
+        }
+    }
     val differ = AsyncListDiffer(this, differCallback)
 
 
@@ -40,24 +40,24 @@ class NewsAdapter: RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
     }
 
     override fun getItemCount(): Int {
-       return differ.currentList.size
+        return differ.currentList.size
     }
 
-    private var onItemClickListener :((Article) -> Unit)? = null
+    private var onItemClickListener: ((Article) -> Unit)? = null
 
     override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
-       val article = differ.currentList[position]
+        val article = differ.currentList[position]
 
         articleImage = holder.itemView.findViewById(R.id.articleImage)
         articleImage = holder.itemView.findViewById(R.id.articleImage)
-<<<<<<< HEAD
-        articlesource = holder
-=======
         articleSource = holder.itemView.findViewById(R.id.articleSource)
         articleTitle = holder.itemView.findViewById(R.id.articleTitle)
         articleDescription = holder.itemView.findViewById(R.id.articleDescription)
         articleDateTime = holder.itemView.findViewById(R.id.articleDateTime)
->>>>>>> a3c805259d4fb06ce10c79083f0913fcc550062d
+
+        holder.itemView.apply {
+
+        }
     }
 
 }

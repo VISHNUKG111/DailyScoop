650f3854b0aabc74941b5912726c0b7b4488bf43
Ren-Jacob-John <141657080+Ren-Jacob-John@users.noreply.github.com>
2024-08-01 17:13:48 +0530

    add

--- app/src/main/java/com/example/dailyscoop/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/example/dailyscoop/ui/fragments/ArticleFragment.kt
@@ -1,34 +1,41 @@
 package com.example.dailyscoop.ui.fragments
 
+import android.os.Bundle
+import android.view.View
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.example.dailyscoop.R
+import com.example.dailyscoop.databinding.FragmentArticleBinding
+import com.example.dailyscoop.ui.NewsActivity
+import com.example.dailyscoop.ui.NewsViewModel
 import com.google.android.material.snackbar.Snackbar
 
 
 class ArticleFragment : Fragment(R.layout.fragment_article) {
 
-    lateinit var newsViewmodel:
+    lateinit var newsViewModel: NewsViewModel
     val args: ArticleFragmentArgs by navArgs()
     lateinit var binding: FragmentArticleBinding
 
-    override fun onViewCreated(view, savedInstanceState: bundle?) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentArticleBinding.bind(view)
 
-        NewsViewsModel = (activity as NewsActivity)newsViewModel
+        newsViewModel = (activity as NewsActivity).newsViewModel
         val article = args.article
 
-        binding.webview.apply {
-            webViewClient = WebVewClient()
-            article.url.let {
-                loadurl(it)
+        binding.webView.apply {
+            webViewClient = WebViewClient()
+            article.url?.let {
+                loadUrl(it)
             }
         }
 
+
         binding.fab.setOnClickListener{
-            newsViewmodel.addToFavourites(article)
-            Snackbar.make(view, "added to favourites", Snackbar.make(view, text:"added to favourites", Snackbar.LENGTH_SHORT)Show()
+            newsViewModel.addToFavorites(article)
+            Snackbar.make(view, "Added to Bookmark", Snackbar.LENGTH_SHORT).show()
         }
     }
 

7cffb6070ae5d4403cbdd408b7b766b8e0f6e52b
Ren-Jacob-John <141657080+Ren-Jacob-John@users.noreply.github.com>
2024-08-01 23:12:58 +0530

    Update BookmarkFragment.kt

--- app/src/main/java/com/example/dailyscoop/ui/fragments/BookmarkFragment.kt
+++ app/src/main/java/com/example/dailyscoop/ui/fragments/BookmarkFragment.kt
@@ -1,4 +1,15 @@
 package com.example.dailyscoop.ui.fragments
 
+
+import com.example.dailyscoop.adaptors.NewsAdapter
+import com.example.dailyscoop.databinding.FragmentBookmarkBinding
+import com.example.dailyscoop.ui.NewsViewModel
+
 class BookmarkFragment {
+
+    lateinit var newsViewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
+    lateinit var binding: FragmentBookmarkBinding
+
+
 }

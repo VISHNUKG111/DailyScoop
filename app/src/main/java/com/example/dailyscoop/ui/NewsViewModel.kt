b48785f20de989d7a827d3ab01aea4b0bd2cb50a
Ren-Jacob-John <141657080+Ren-Jacob-John@users.noreply.github.com>
2024-08-01 21:22:35 +0530

    Update NewsViewModel.kt

--- app/src/main/java/com/example/dailyscoop/ui/NewsViewModel.kt
+++ app/src/main/java/com/example/dailyscoop/ui/NewsViewModel.kt
@@ -64,7 +64,7 @@ class NewsViewModel(app: Application, val newsRepository: NewsRepository): Andro
         return Resource.Error(response.message())
     }
 
-    fun addToFavorites(article: Article) = viewModelScope.launch {
+    fun addToBookmark(article: Article) = viewModelScope.launch {
         newsRepository.upsert(article)
     }
 

12c2e6a14beb0e8edf4cdfeec625080cdee1b77b
VISHNUKG111 <vishnukgirish1@gmail.com>
2024-08-02 13:18:29 +0530

    add

--- app/src/main/java/com/example/dailyscoop/ui/fragments/HeadlinesFragment.kt
+++ app/src/main/java/com/example/dailyscoop/ui/fragments/HeadlinesFragment.kt
@@ -1,10 +1,16 @@
 package com.example.dailyscoop.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.dailyscoop.adaptors.NewsAdapter
+import com.example.dailyscoop.databinding.FragmentArticleBinding
+import com.example.dailyscoop.databinding.FragmentHeadlinesBinding
 import com.example.dailyscoop.ui.NewsViewModel
 import java.lang.Error
 
@@ -14,4 +20,63 @@ class HeadlinesFragment : Fragment() {
     lateinit var retryButton: Button
     lateinit var errorText: TextView
     lateinit var itemHeadlinesError: CardView
+    lateinit var binding:FragmentHeadlinesBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentHeadlinesBinding.bind(view)
+
+
+    }
+
+    var isError = false
+    var isLoading = false
+    var isLastPage = false
+    var isScrolling = false
+
+    private fun hideProgressBar(){
+
+        binding.paginationProgressBar.visibility = View.INVISIBLE
+        isLoading = false
+    }
+
+    private fun showProgressBar(){
+        binding.paginationProgressBar.visibility = View.VISIBLE
+        isLoading = true
+
+    }
+
+    private  fun hideErrorMessage(){
+        itemHeadlinesError.visibility = View.INVISIBLE
+        isError = false
+    }
+
+    private  fun showErrorMessage(message:String){
+        itemHeadlinesError.visibility = View.VISIBLE
+        errorText.text = message
+        isError = true
+    }
+
+    val scrollListener = object : RecyclerView.OnScrollListener(){
+
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+
+            val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+            val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
+            val visibleItemCount = layoutManager.childCount
+            val totalItemCount = layoutManager.itemCount
+
+            val isNoErrors = !isError
+            val isNotLoadingAndNotLastPage = !isLoading && !isLastPage
+            val isAtLastItem =firstVisibleItemPosition + visibleItemCount >= totalItemCount
+            val isNotAtBeginning = firstVisibleItemPosition >= 0
+        }
+
+        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+            super.onScrollStateChanged(recyclerView, newState)
+        }
+    }
+
+
 }

b57aa79ab9e8e6099414ec1b35cb7a54e89fc80b
Ren-Jacob-John <141657080+Ren-Jacob-John@users.noreply.github.com>
2024-08-03 14:27:36 +0530

    Update BookmarkFragment.kt

--- app/src/main/java/com/example/dailyscoop/ui/fragments/BookmarkFragment.kt
+++ app/src/main/java/com/example/dailyscoop/ui/fragments/BookmarkFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
